9872d96cab4ce6613940906df7454885ccd9b86e
Md Alamin <alamin3636md@gmail.com>
2024-09-21 14:49:00 +0600

    addmoney complete

--- js/home.js
+++ js/home.js
@@ -4,4 +4,17 @@ document.getElementById('btn-add-money').addEventListener('click',function(event
     console.log(addMoney);
     const addInput = document.getElementById('input-pin-number').value;
     console.log(addInput);
+    if(addInput === '1234'){
+        console.log("add money to your account");
+        const balance = document.getElementById('account-balance').innerText;
+        console.log(balance)
+         let a =parseFloat(balance);
+         let b = parseFloat(addMoney);
+      const totalbalance =a + b;
+      console.log(totalbalance);
+        document.getElementById('account-balance').innerText = totalbalance;
+
+    }else{
+        alert("please try again mc")
+    }
 })
