2e05bc6c9004b047d830feb5876097c91bb4d2a2
Md Alamin <alamin3636md@gmail.com>
2024-09-21 14:14:56 +0600

    input fiels done

--- js/home.js
+++ js/home.js
@@ -1,1 +1,7 @@
-console.log('home agayaa')
+document.getElementById('btn-add-money').addEventListener('click',function(event){
+    event.preventDefault();
+    const addMoney = document.getElementById('input-add-money').value;
+    console.log(addMoney);
+    const addInput = document.getElementById('input-pin-number').value;
+    console.log(addInput);
+})
